f6addf097d2bb440f38fee9228933effdd07ad1c
Niroj <rossivx@gmail.com>
2017-03-17 12:29:59 +0545

    add list datatype.py

---
+++ datatype.py
@@ -0,0 +1,9 @@
+an_integer=6
+a_floating_point=17.60
+a_boolean=true
+a_string="foo"
+
+print("integer value=", an_integer)
+print("float value=",a_floating_point)
+print("boolean value=",a_boolean)
+print("string value=",a_string)
