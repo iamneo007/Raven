9b8c4ff0ea9fa553559327e0a59abb7a20981c71
Niroj <rossivx@gmail.com>
2017-03-17 13:15:12 +0545

    add list input.py

---
+++ input.py
@@ -0,0 +1,9 @@
+name = input("please enter your name:")
+address = input("enter your address:")
+Gender = input("enter your Gender:")
+
+print("Hi",name)
+print("your address is",address)
+print("you are",Gender)
+
+print("Thanku for your kind information")
