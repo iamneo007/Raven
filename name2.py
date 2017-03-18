1915f6878b3addfe1b5720c6bdba3760a7eae84e
Niroj <rossivx@gmail.com>
2017-03-18 09:54:56 +0545

    add list name3.py

---
+++ name2.py
@@ -0,0 +1,5 @@
+PI = 3.1415
+radius = input('Enter radius of circle(meters):')
+area = PI * float(radius) ** 2
+
+print("\nmeters of circle = % .2f sq. meters" % area)
---
+++ name3.py
@@ -0,0 +1,11 @@
+first_name = input('your first name :')
+last_name = input('your last name :')
+
+#old style formatting
+print('hello %s %s!' % (first_name, last_name))
+print('hello {0} {1}!' .format(first_name,last_name))
+
+
+print('hello {1} {0}!'.format(first_name,last_name))
+print('hello {0} {0} {1}!'.format(first_name,last_name))
+
