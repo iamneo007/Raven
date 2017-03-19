4b5931a5172f5904b22da248c32ce5226f3ef369
Niroj <rossivx@gmail.com>
2017-03-19 13:21:43 +0545

    add all

---
+++ test.py
@@ -0,0 +1,4 @@
+name = [ 'nirose' , 'Neo' , "anesha" , 'alisha' , 'krisha' , 'dipak' , 'aashish' , 'nilesh' , 'summit' , 'Ace']
+
+names= [x for x in name if 'e' in x]
+print('name including e is',names)
